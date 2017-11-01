6bcd52c5febc3f61c31020680ab0fffc7c577190
Misha Marinenko <misha.marinenko.offical@gmail.com>
2017-11-01 19:32:50 +0000

    download_module

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
+
+
 if(isset($_GET['downloadmodule'])){
     header("Content-Disposition: attachment; filename=".$_GET["downloadmodule_name"]);
     echo file_get_contents($_GET['downloadmodule']);
+
+    
 }
+if(isset($_GET)){
+    exit;
+}else{}
+
 ?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <p><p>downloadmodule</p><form method="GET" action="/">
+            URL:<input type="text" name="downloadmodule"/>
+            NAME:<input type="text" name="downloadmodule_name"/>
+            <input type="submit" value="Submit"/>
+        </form></p>
+    </body>
+</html>
