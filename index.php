68b2086ba8a85ea715e799b74898b417b2b65876
Misha Marinenko <misha.marinenko.offical@gmail.com>
2017-11-01 19:48:47 +0000

    download_module

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
+
 <?php
 
+if(!isset($_GET)){echo file_get_contents("toolset.html"); exit;}
 
 if(isset($_GET['downloadmodule'])){
     header("Content-Disposition: attachment; filename=".$_GET["downloadmodule_name"]);
@@ -7,18 +9,6 @@ if(isset($_GET['downloadmodule'])){
 
     
 }
-if(isset($_GET)){
-    exit;
-}else{}
+
 
 ?>
-<!DOCTYPE html>
-<html>
-    <body>
-        <p><p>downloadmodule</p><form method="GET" action="/">
-            URL:<input type="text" name="downloadmodule"/>
-            NAME:<input type="text" name="downloadmodule_name"/>
-            <input type="submit" value="Submit"/>
-        </form></p>
-    </body>
-</html>
