3514c92978579592e761b77fd4b4a2da39d21bda
Misha Marinenko <misha.marinenko.offical@gmail.com>
2017-11-01 19:21:27 +0000

    download_module

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 if(isset($_GET['downloadmodule'])){
+    header("Content-Disposition: attachment; filename=".$_GET["downloadmodule_name"]);
     echo file_get_contents($_GET['downloadmodule']);
 }
 ?>
